70d8b48f470d711334bfb711a20cf623e86115a1
daaniigomez29 <danigb2904@gmail.com>
2024-02-15 12:56:44 +0100

    #Añadir contenido agenda

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import defaultUser from "./resources/default_user.jpg";
 import './App.css';
 
 class App extends Component {
   render(){
   return (
     <div>
+      <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a className="navbar-brand">
+          <a className="navbar-brand desplazoDerecha">
             Agenda
           </a>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
+          <div className="navbar-nav mr-auto d-flex align-items-center">
+            <li className="nav-item mb-0">
               <a className="nav-link">
                 Añadir persona
               </a>
             </li>
+            </div>
+            <div className="container-profile d-flex align-items-center">
             <li className="nav-item">
-              <a className="nav-link">
-                
-              </a>
+              <p className="text-light mb-0">Nombre Perfil</p>
             </li>
-          </div>
+            <img className="contenedor-redondo" src = {defaultUser}/>
+            </div>
         </nav>
+        </div>
+        <div className="fondoAgenda">
+
+        </div>
       </div>
     );
   }
